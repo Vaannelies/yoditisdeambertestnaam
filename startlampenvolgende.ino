4b45a427589656ec37751e6da4264c3336dafc24
Annelies <43701941+Vaannelies@users.noreply.github.com>
2019-04-09 13:58:34 +0200

    Ik heb de tekst aangepast
    Er staat yoyoyoyoyoyo tussen de tekst, die tekst was toch al niet belangrijk dus het is niet erg dat het een beetje weg is

--- startlampenvolgende.ino
+++ startlampenvolgende.ino
@@ -11,12 +11,9 @@
 
  
 // Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+// Payoyoyoyoyoyoy for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products WS2812 LEDs)
+//   NEO_KHZ800  800 KHzyoyoyoyo bitstream (most NeoPixel products WS2812 LEDs)
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
 int hinkelmodus()
